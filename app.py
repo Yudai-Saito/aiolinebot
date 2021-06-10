44d19b399e21af43fe14e1578e69558371bea71e
YudaiSaito <yudai.saito.dev@gmail.com>
2021-06-10 17:16:29 +0900

    responderで実装。 38行目でコメントエラー。

--- app.py
+++ app.py
@@ -2,45 +2,51 @@ import responder
 import json
 import os
 
-from linebot import LineBotApi, WebhookHandler
+import time
+import random
+
+import asyncio
+
+import logging.config
+
+from aiolinebot import AioLineBotApi
+
+from linebot import LineBotApi
+from linebot.webhook import WebhookHandlerV2
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 api = responder.API()
 
-line_bot_api = LineBotApi(os.environ["LINE_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+line_bot_api = AioLineBotApi(os.environ["LINE_ACCESS_TOKEN"])
+handler = WebhookHandlerV2(os.environ["LINE_CHANNEL_SECRET"])
 
+@api.route("/callback")
+async def on_post(req, resp):
+    @api.background.task
+    async def handles(body, signature):
+        await handler.handle_async(body, signature)
 
-class CallBack:
-    async def on_post(self, req, resp):
+    signature = req.headers['X-Line-Signature']
+    
+    body = await req.media()
+    body = json.dumps(body, ensure_ascii=False).replace(' ', '')
 
-        @api.background.task
-        def handles():
-            handler.handle(body, signature)
-        
-        signature = req.headers['X-Line-Signature']
-
-        body = await req.media()
-        body = json.dumps(body, ensure_ascii=False).replace(' ', '')
-
-        try:
-            handles()
-            resp.status_code = 200
-            resp.text = 'OK'
-        except InvalidSignatureError as e:
-            print(e)
-            resp.status_code = 400
-            resp.text = e
+    try:
+        resp.status_code = 200
+        resp.text = 'OK'
+        #TypeError: An asyncio.Future, a coroutine or an awaitable is required
+        await handles(body, signature)
+    except InvalidSignatureError as e:
+        resp.status_code = 400
+        resp.text = e
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
-
-
-api.add_route("/callback", CallBack)
+async def handle_message(event):
+    await line_bot_api.reply_message_async(event.reply_token, TextSendMessage(text=event.message.text))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get("PORT", 8000))
+
     api.run(address='0.0.0.0', port=port, debug=True)
